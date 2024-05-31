01f4323d04a4bae2638e56ec3dcf46126a6f2ff9
ManoharJunga <95352064+ManoharJunga@users.noreply.github.com>
2024-05-31 14:37:20 +0530

    commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Text, View } from 'react-native'
+import { Link } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
 
 const index = () => {
   return (
-    <View>
-      <Text>index</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">index</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: 'blue'}}>Go to Profile</Link>
     </View>
   )
 }
 
 export default index
 
-const styles = StyleSheet.create({})
